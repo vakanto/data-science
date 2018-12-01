8f3ec524725feea062bb469d3f1286a6745d96f2
vakanto <vakanto@posteo.eu>
2018-12-02 00:21:43 +0100

    pyplot implementation

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,22 @@
 from __future__ import division
 import math as m
+import time
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-input=[[1,5],[6,2],[8,1],[3,5],[2,4],[2,6],[6,1],[6,8],[7,3],[7,6],[8,3],[8,7]]
-k=3
+#input=[[1,5],[6,2],[8,1],[3,5],[2,4],[2,6],[6,1],[6,8],[7,3],[7,6],[8,3],[8,7],[3,4],[2,9],[6,7],[2,1]]
+input=[]
+for i in range(1000):
+    a=random.randint(0, 1000)
+    b=random.randint(0, 1000)
+    l=[a,b]
+    input.append(l)
+k=5
 
+def euclideanDist(a,b):
+    result=m.sqrt(sum([m.pow((x-y),2) for x,y in zip(a,b)]))
+    return result
 
 class Cluster():
     def __init__(self):
@@ -12,17 +25,21 @@ class Cluster():
 
     def calculateCentroid(self):
         temp = [sum(x) for x in zip(*self.elements)]
-        print zip(*self.elements)
+      #  print zip(*self.elements)
         centroid = [x / (len(self.elements)) for x in temp]
         self.centroid = centroid
+
+    def compactness(self, dist_fun=euclideanDist):
+        c=0
+        for x in self.elements: 
+           # print("Vektor :" , x , " ", " Centroid: ", self.centroid)
+            if(len(self.centroid)==0):
+                print("Fail!")
+            m=euclideanDist(x, self.centroid)
+            m=m*m
+            c=c+m
+        return c    
     
-
-def euclideanDist(a,b):
-    result=m.sqrt(sum([m.pow((x-y),2) for x,y in zip(a,b)]))
-    #print result
-    #print result
-    return result
-
 class kmeansClusterer():
 
     def __init__(self):
@@ -32,7 +49,6 @@ class kmeansClusterer():
         for i in range(k):
             self.clusters.append(Cluster())
             self.clusters[i].centroid=input[i]
-
         for x in input:
             pre=-1
             c=None
@@ -51,29 +67,52 @@ class kmeansClusterer():
             self.clusters[i].calculateCentroid()
 
     def calculateCluster(self):
-        for x in range(5):
+        for x in range(10):
             for c in self.clusters:
                 for t in c.elements:
+                    new=c
                     for d in self.clusters:
                         dist1=euclideanDist(d.centroid, t)
                         dist2=euclideanDist(c.centroid, t)
-
-                        print(x, "" , c.centroid ," " ,d.centroid, " ",t , " ", dist1, " ", dist2)
+                        #print(x, "" , c.centroid ," " ,d.centroid, " ",t , " ", dist1, " ", dist2)
                         if dist1<dist2:
-                            d.elements.append(t)
-                            c.elements.remove(t)
-                            d.calculateCentroid()
+                           new=d
+                    new.elements.append(t)
+                    c.elements.remove(t)
+                    new.calculateCentroid()
+            clusterPlot(self.clusters)
+        for i in range(k):
+            self.clusters[i].calculateCentroid()
+
+
+
+
+
+
+
+
+
+def clusterPlot(clusters):
+    import matplotlib.pyplot as plot
+    import numpy as np
+
+    #fig = plot.figure()
+    
+    for c in clusters:
+        elems = np.array(c.elements)
+        plt.scatter(elems[:,0], elems[:,1])
+        centroid = c.centroid
+        plt.plot(centroid[0], centroid[1], 'X', c='black')
+        fig.canvas.draw()
+
 
 alg=kmeansClusterer()
 alg.initializeCluster()
-#for i in range(k):
-#    print(alg.clusters[i].centroid, " ",alg.clusters[i].elements)
-
+i=-1
+plt.ion()
+fig = plt.figure()
+plt.show()
+clusterPlot(alg.clusters)
 alg.calculateCluster()
-for i in range(k):
-    print(alg.clusters[i].centroid, " ",alg.clusters[i].elements)
-#print(alg.clusters[0].elements)
-#print(alg.clusters[1].elements)
-#print(alg.clusters[2].elements)
-
-    
+# # for i in range(k):
+# #     print(alg.clusters[i].centroid, " ",alg.clusters[i].elements)
